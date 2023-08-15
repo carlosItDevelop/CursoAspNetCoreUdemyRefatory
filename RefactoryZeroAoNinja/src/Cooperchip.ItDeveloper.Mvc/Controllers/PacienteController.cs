f19da20b0f4f6cb3e9d0347d90300d9d92ee7850
Carlos A Santos <carlos.itdeveloper@gmail.com>
2023-08-15 15:15:35 -0300

    Add List<Paciente> in Index on PacienteController

--- RefactoryZeroAoNinja/src/Cooperchip.ItDeveloper.Mvc/Controllers/PacienteController.cs
+++ RefactoryZeroAoNinja/src/Cooperchip.ItDeveloper.Mvc/Controllers/PacienteController.cs
@@ -1,29 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cooperchip.ItDeveloper.Domain.Entities;
+using Cooperchip.ItDeveloper.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections;
 
 namespace Cooperchip.ItDeveloper.Mvc.Controllers
 {
-    [Route("gestao-de-paciente")]
-    [Route("gestao-de-pacientes")]
+
     public class PacienteController : BaseController
     {
-        [Route("pacientes")]
-        [Route("obter-pacientes")]
-        public IActionResult Index()
+        private readonly ILogger _logger;
+        private readonly ITDeveloperDbContext _context;
+
+        public PacienteController(ITDeveloperDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        //[HttpGet("detalhes-de-paciente/{id:guid}")]
-        [HttpGet("detalhes-de-paciente/{id}")]
-        public IActionResult DetalhesDePaciente(string id)
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<PacienteViewModel> listView = await MapperModelsToViewModels();
+
+            return View(listView);
         }
 
 
 
+
+        #region: Mapper de List<Model> para List<ViewModel>
+        private async Task<List<PacienteViewModel>> MapperModelsToViewModels()
+        {
+            var pacientes = await _context.Paciente
+                .Include(x => x.EstadoPaciente)
+                .AsNoTracking().ToListAsync();
+
+            List<PacienteViewModel> listView = new();
+
+            foreach (var item in pacientes)
+            {
+                listView.Add(new PacienteViewModel
+                {
+                    Ativo = item.Ativo,
+                    Cpf = item.Cpf,
+                    DataInternacao = item.DataInternacao,
+                    DataNascimento = item.DataNascimento,
+                    Email = item.Email,
+                    EstadoPaciente = item.EstadoPaciente,
+                    EstadoPacienteId = item.EstadoPacienteId,
+                    Id = item.Id,
+                    Nome = item.Nome,
+                    Rg = item.Rg,
+                    RgDataEmissao = item.RgDataEmissao,
+                    RgOrgao = item.RgOrgao,
+                    Sexo = item.Sexo,
+                    TipoPaciente = item.TipoPaciente,
+                    Motivo = item.Motivo
+                });
+            }
+
+            return listView;
+        }
+        #endregion
     }
-
 
 }
